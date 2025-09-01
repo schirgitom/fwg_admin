83ac0817dae0e80fac673ad28c291acedf0b44eb
Thomas Schirgi <thomas.schirgi@schischo.com>
2025-09-01 17:44:39 +0200

    Init

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Mein Projekt</title>
+    <!-- Bootstrap CSS -->
+    <link href="node_modules/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+    <h1 class="text-center">Hallo mit Bootstrap!</h1>
+    <button class="btn btn-primary">Klick mich</button>
+</div>
+
+<!-- Bootstrap JS + Popper -->
+<script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+</body>
+</html>
